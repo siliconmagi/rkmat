19e4a8629348efcab8f78ddf9f93ed0eb3e41695
siliconmagi <siliconmagi@yandex.com>
2016-10-07 23:38:30 -0700

    Appbar links history init

--- src/components/Appbar/Appbar.js
+++ src/components/Appbar/Appbar.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import history from '../../core/history';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import AppBar from 'material-ui/AppBar';
+import Avatar from 'material-ui/Avatar';
+import ActionHome from 'material-ui/svg-icons/action/home';
+import ActionAssignment from 'material-ui/svg-icons/action/assignment';
+import ActionFace from 'material-ui/svg-icons/action/face';
+import ActionFingerprint from 'material-ui/svg-icons/action/fingerprint';
+import ActionSettings from 'material-ui/svg-icons/action/settings';
+import Link from '../Link';
 
 export default class Appbar extends React.Component {
 
@@ -18,7 +26,11 @@ export default class Appbar extends React.Component {
     return (
       <div>
         <AppBar
-        title="Title"
+        title="Silicon Magi"
+        iconElementRight={
+           <Avatar
+             src='http://cdn.rawgit.com/siliconmagi/pictures/master/atar.svg'
+        />}
         onLeftIconButtonTouchTap={this.handleToggle}
         style={{
         position: 'fixed'
@@ -30,8 +42,36 @@ export default class Appbar extends React.Component {
           open={this.state.open}
           onRequestChange={(open) => this.setState({open})}
         >
-          <MenuItem onTouchTap={this.handleClose}>Menu Item</MenuItem>
-          <MenuItem onTouchTap={this.handleClose}>Menu Item 2</MenuItem>
+          <MenuItem onTouchTap={this.handleClose}
+          containerElement={<Link to="/" />}
+          primaryText="Home"
+          leftIcon={
+             <ActionHome />}
+          />
+          <MenuItem onTouchTap={this.handleClose}
+          containerElement={<Link to="/about" />}
+          primaryText="About"
+          leftIcon={
+             <ActionAssignment />}
+          />
+          <MenuItem onTouchTap={this.handleClose}
+          containerElement={<Link to="/contact" />}
+          primaryText="Contact"
+          leftIcon={
+             <ActionFace />}
+          />
+          <MenuItem onTouchTap={this.handleClose}
+          containerElement={<Link to="/login" />}
+          primaryText="Login"
+          leftIcon={
+             <ActionFingerprint />}
+          />
+          <MenuItem onTouchTap={this.handleClose}
+          containerElement={<Link to="/register" />}
+          primaryText="Register"
+          leftIcon={
+             <ActionSettings />}
+          />
         </Drawer>
       </div>
     );
